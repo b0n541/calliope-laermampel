136d218a4fcd1f72aa524c4f73084e190d419aac
Jan Schäfer <j@nschaefer.net>
2023-01-07 10:11:51 +0100

    Erste Version der Lärmampel

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let lautstaerke = 0
 basic.forever(function () {
-	
+    lautstaerke = input.soundLevel()
+    if (lautstaerke > 180) {
+        basic.setLedColor(0xff0000)
+    } else if (lautstaerke > 75) {
+        basic.setLedColor(0xffff00)
+    } else {
+        basic.setLedColor(0x00ff00)
+    }
+    basic.pause(100)
 })
